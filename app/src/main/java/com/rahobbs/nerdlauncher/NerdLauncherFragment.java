eecb2f4c7d62d153a1ca947628ad44a65d6d02a8
rahobbs <hobbs.rachael@gmail.com>
2017-03-22 19:11:21 -0400

    Initial commit

---
+++ app/src/main/java/com/rahobbs/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,31 @@
+package com.rahobbs.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rachael on 3/22/17.
+ */
+
+public class NerdLauncherFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+
+    public static NerdLauncherFragment newInstance() {
+        return new NerdLauncherFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return v;
+    }
+}
